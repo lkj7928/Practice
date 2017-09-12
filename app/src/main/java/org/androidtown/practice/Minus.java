43e610abbafcd4da8ecdd2df3a842c97a1d05bc3
dreamline91 <dreamline91@naver.com>
2017-09-12 22:11:46 +0900

    add minus function

---
+++ app/src/main/java/org/androidtown/practice/Minus.java
@@ -0,0 +1,11 @@
+package org.androidtown.practice;
+
+/**
+ * Created by dream on 2017-09-12.
+ */
+
+public class Minus {
+    public static int operator(int a, int b){
+        return a-b;
+    }
+}
